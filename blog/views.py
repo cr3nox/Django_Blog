b74650ccf38f79d18a8592fa869bb82c971c5d37
andreash <andreas.hofmann@hs-augsburg.de>
2019-12-22 21:50:47 +0100

    Part 5

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Andreas',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'December 22, 2019'
-    },
-    {
-        'author': 'Dennis',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'December 23, 2019'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
