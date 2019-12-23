6938f8b7610a11769e9be27208ca1473c2919ed6
andreash <andreas.hofmann@hs-augsburg.de>
2019-12-23 16:43:02 +0100

    Part 8 - User Profile and Picture

--- Django_Blog/settings.py
+++ Django_Blog/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Crispy using Bootstrap 4
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
